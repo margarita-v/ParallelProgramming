b7091c4b4497caac6359a8ceab94df15e2b00f7b
Margarita <margo.himera@yandex.ru>
2018-12-03 22:00:39 +0300

    Initial solve for task 3 was added

--- task3/task3_num6.cpp
+++ task3/task3_num6.cpp
@@ -1,5 +1,11 @@
+#include <mpi.h>
+#include <string>
 #include <iostream>
-#include "mpi.h"
+
+using namespace std;
+
+#define m 7
+#define n 7
 
 /**
  * УСЛОВИЕ ЗАДАЧИ:
@@ -14,21 +20,71 @@
  * ... ... ... ...
  * am1 am2 ... Amn
  */
+
 const int MASTER_ID = 0; // Нулевой процесс
+const int WORKER_ID = 1;
+
+const string BEFORE_MATRIX_MESSAGE = "Matrix before:";
+const string AFTER_MATRIX_MESSAGE = "Matrix after:";
+
+void printMatrix(int (*arr)[n], string message);
 
 int main(int argc, char *argv[]) {
 
-    int rank, processCount;
+    int rank, tag = MASTER_ID;
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &processCount);
 
-    if (rank == MASTER_ID) {
-        //todo
-    } else {
-        //todo
+    int blocklength[m - 1];
+    int displacement[m - 1];
+
+    blocklength[0] = n + 2;
+    for (int i = 1; i < m - 2; i++) {
+        blocklength[i] = 3;
+    }
+    blocklength[m - 2] = n + 1;
+
+    displacement[0] = 0;
+    for (int i = 1; i < m - 1; i++) {
+        displacement[i] = (i + 1) * n - 1;
     }
 
+    MPI_Datatype customMatrixType;
+    MPI_Type_indexed(m - 1, blocklength, displacement, MPI_INT, &customMatrixType);
+    MPI_Type_commit(&customMatrixType);
+
+    int matrix[m][n];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = rank;
+        }
+    }
+
+    if (rank == MASTER_ID) {
+        MPI_Send(&matrix[0][0], 1, customMatrixType, WORKER_ID, tag, MPI_COMM_WORLD);
+    } else if (rank == WORKER_ID) {
+        printMatrix(matrix, BEFORE_MATRIX_MESSAGE);
+        MPI_Status status;
+        MPI_Recv(&matrix[0][0], 1, customMatrixType, MASTER_ID, tag, MPI_COMM_WORLD, &status);
+        printMatrix(matrix, AFTER_MATRIX_MESSAGE);
+    }
+
+    MPI_Type_free(&customMatrixType);
     MPI_Finalize();
     return 0;
+}
+
+/**
+ * Функция для печати матрицы и ее описания
+ */
+void printMatrix(int matrix[m][n], string description) {
+    cout << description << endl;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << to_string(matrix[i][j]) << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
